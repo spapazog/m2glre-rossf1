08b9cfa607870ee680bce784cfc84240136565d8
Simon Joussellin <sim.joussellin@gmail.com>
2012-05-03 11:02:13 +0200

    Add a test which fail

--- RomanianOpenSourceSimonFull100pui/src/test/java/ups/m2glre/rossf1/AppTest.java
+++ RomanianOpenSourceSimonFull100pui/src/test/java/ups/m2glre/rossf1/AppTest.java
@@ -1,38 +1,19 @@
 package ups.m2glre.rossf1;
 
-import universite.toulouse.moodlexmlapi.core.data.QuestionError;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName
-     *            name of the test case
-     */
-    public AppTest(final String testName) {
-        super(testName);
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
+	@Test
+	public void testQuizzExist() {
+		
+		fail();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public final void testApp() {
-        QuestionError questionError = new QuestionError("type not supported",
-                "the question type is not supported");
-        System.out.println(questionError.getDescription());
-        assertEquals("type not supported", questionError.getCode());
-    }
+	}
+
 }
