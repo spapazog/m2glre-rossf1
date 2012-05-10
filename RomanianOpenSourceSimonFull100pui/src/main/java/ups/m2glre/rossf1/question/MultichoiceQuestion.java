1194163a9dbfa9c04bb9e9e11ab0211d2515649a
Simon Joussellin <sim.joussellin@gmail.com>
2012-05-10 12:01:31 +0200

    test pour prouvé à Stephane que ca marche

---
+++ RomanianOpenSourceSimonFull100pui/src/main/java/ups/m2glre/rossf1/question/MultichoiceQuestion.java
@@ -0,0 +1,68 @@
+package ups.m2glre.rossf1.question;
+
+import java.util.List;
+
+import universite.toulouse.moodlexmlapi.core.data.Question;
+import universite.toulouse.moodlexmlapi.core.data.QuestionError;
+import universite.toulouse.moodlexmlapi.core.data.QuestionText;
+import universite.toulouse.moodlexmlapi.core.data.QuestionType;
+
+/**
+ * 
+ * @author Simon J
+ *
+ */
+
+public class MultichoiceQuestion implements Question{
+
+	public Float getDefaultGrade() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	public List<QuestionError> getErrors() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	public String getGeneralFeedBack() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	public String getImageBase64() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	public String getImageUrl() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	public String getName() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	public Float getPenalty() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	public QuestionText getQuestionText() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	public QuestionType getQuestionType() {
+	    // TODO Auto-generated method stub
+	    return QuestionType.multichoice;
+    }
+
+	public Boolean isHidden() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+}
