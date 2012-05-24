8c5486fd401fe86f6133590c2541bb4171edd00d
Stephane P <steph@100pui.com>
2012-05-24 09:23:15 +0200

    Début d'implémentation du QuizParser [#7]

--- RomanianOpenSourceSimonFull100pui/src/main/java/ups/m2glre/rossf1/parser/QuizParser.java
+++ RomanianOpenSourceSimonFull100pui/src/main/java/ups/m2glre/rossf1/parser/QuizParser.java
@@ -1,5 +1,8 @@
 package ups.m2glre.rossf1.parser;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.jdom.Element;
 
 /**
@@ -12,7 +15,16 @@ public class QuizParser {
      * Constructeur du Quiz
      * @param quizRoot node du quizz
      */
-    public QuizParser(Element quizRoot) {
-        
+    public QuizParser(final Element quizRoot) {
+        //Création de la liste de questions
+        List listQuestions = quizRoot.getChildren("question");
+        Iterator i = listQuestions.iterator();
+
+        while (i.hasNext()) {
+           Element question = (Element) i.next();
+
+           //Affichage du type de la question (GABRIEL A TOIIIIIII !)
+           System.out.println(question.getAttributeValue("category"));
+        }
     }
 }
