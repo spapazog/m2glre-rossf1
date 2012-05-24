d6fbc210903e5cc720df9722b0e9cb222e63f459
Stephane P <steph@100pui.com>
2012-05-24 16:15:46 +0200

    Ajout de levée d'exception InvalidQuizFormatException [#11]

--- RomanianOpenSourceSimonFull100pui/src/main/java/ups/m2glre/rossf1/parser/NumericalQuestionParser.java
+++ RomanianOpenSourceSimonFull100pui/src/main/java/ups/m2glre/rossf1/parser/NumericalQuestionParser.java
@@ -2,35 +2,41 @@ package ups.m2glre.rossf1.parser;
 
 import org.jdom.Element;
 
+import universite.toulouse.moodlexmlapi.core.InvalidQuizFormatException;
 import ups.m2glre.rossf1.question.NumericalQuestion;
 
 public class NumericalQuestionParser extends QuestionParser {
 
-    public void parseSpecializedQuestion(Element questionXML) {
+    public void parseSpecializedQuestion(Element questionXML)
+            throws InvalidQuizFormatException {
         NumericalQuestion q = (NumericalQuestion) question;
 
-        //Parse la fraction
-        q.setAnswerFraction(Integer.valueOf(
-                questionXML.getChild("answer").getAttributeValue("fraction")));
-        //Parse la valeur
-        q.setAnswerValue(Integer.valueOf(
-                questionXML.getChild("answer").getChild("text").getValue()));
-        //Parse la tolerence
-        q.setAnswerTolerence(Integer.valueOf(
-                questionXML.getChild("answer").getChild("tolerance").getValue()));
-        //Parse l'unité
-        q.setAnswerUnitMultiplier(Integer.valueOf(
-                questionXML.getChild("units").getChild("unit").
-                getChild("multiplier").getValue()));
-        //Parse le texte de l'unité
-        q.setAnswerUnitName(questionXML.getChild("units").getChild("unit").
-                getChild("unit_name").getValue());
-        //Parse la answer shuffle
-        q.setAnswerShuffle(Integer.valueOf(
-                questionXML.getChild("shuffleanswers").getValue()) == 1);
-      //Parse le feedback text
-        q.setFeedbackText(questionXML.getChild("answer").getChild("feedback").
-                getChild("text").getValue());
+        try {
+            //Parse la fraction
+            q.setAnswerFraction(Integer.valueOf(
+                    questionXML.getChild("answer").getAttributeValue("fraction")));
+            //Parse la valeur
+            q.setAnswerValue(Integer.valueOf(
+                    questionXML.getChild("answer").getChild("text").getValue()));
+            //Parse la tolerence
+            q.setAnswerTolerence(Integer.valueOf(
+                    questionXML.getChild("answer").getChild("tolerance").getValue()));
+            //Parse l'unité
+            q.setAnswerUnitMultiplier(Integer.valueOf(
+                    questionXML.getChild("units").getChild("unit").
+                    getChild("multiplier").getValue()));
+            //Parse le texte de l'unité
+            q.setAnswerUnitName(questionXML.getChild("units").getChild("unit").
+                    getChild("unit_name").getValue());
+            //Parse la answer shuffle
+            q.setAnswerShuffle(Integer.valueOf(
+                    questionXML.getChild("shuffleanswers").getValue()) == 1);
+          //Parse le feedback text
+            q.setFeedbackText(questionXML.getChild("answer").getChild("feedback").
+                    getChild("text").getValue());
+        } catch (Exception e) {
+            throw new InvalidQuizFormatException(e.getCause());
+        }
     }
 
 }
