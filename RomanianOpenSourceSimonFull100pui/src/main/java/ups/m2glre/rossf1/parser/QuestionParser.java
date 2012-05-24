30e6bc2cae190704048eb5080f9f85c3d497d42d
Stephane P <steph@100pui.com>
2012-05-24 10:23:32 +0200

    Modification de l'archi générale pour le parseur [#7]

--- RomanianOpenSourceSimonFull100pui/src/main/java/ups/m2glre/rossf1/parser/QuestionParser.java
+++ RomanianOpenSourceSimonFull100pui/src/main/java/ups/m2glre/rossf1/parser/QuestionParser.java
@@ -3,6 +3,7 @@ package ups.m2glre.rossf1.parser;
 import org.jdom.Element;
 
 import universite.toulouse.moodlexmlapi.core.data.Question;
+import ups.m2glre.rossf1.question.GenericQuestion;
 
 /**
  * QuestionParser.
@@ -11,8 +12,33 @@ import universite.toulouse.moodlexmlapi.core.data.Question;
  */
 public abstract class QuestionParser {
     /**
-     * @param question issue du XML
+     * Représentation d'une question
+     */
+    private GenericQuestion question;
+
+    /**
+     * @param questionXML issue du XML
      * @return la question parsée
      */
-    public abstract Question parseQuestion(Element question);
+    public final Question parseQuestion(final Element questionXML) {
+        question = new GenericQuestion();
+        parseGenericField(questionXML);
+        parseSpecializedQuestion(questionXML);
+
+        return question;
+    }
+
+    /**
+     * Méthode de parsage d'une question spéciale à implémenter
+     * @param questionXML issue du XML
+     */
+    public abstract void parseSpecializedQuestion(Element questionXML);
+
+    /**
+     * Méthode de parsage des champs génériques
+     * @param questionXML issue du XML
+     */
+    public final void parseGenericField(final Element questionXML) {
+        question.setName("Test");
+    }
 }
--- RomanianOpenSourceSimonFull100pui/src/main/java/ups/m2glre/rossf1/question/GenericQuestion.java
+++ RomanianOpenSourceSimonFull100pui/src/main/java/ups/m2glre/rossf1/question/GenericQuestion.java
@@ -10,55 +10,132 @@ import universite.toulouse.moodlexmlapi.core.data.QuestionType;
 /**
  * @author steeepph
  */
-public abstract class GenericQuestion implements Question  {
+public class GenericQuestion implements Question  {
+
+    private String name;
+    private QuestionText questionText;
+    private boolean questionHidden;
+    private String feedback;
+    private Float penalty;
+    private Float grade;
+    private QuestionType questionType;
+
+    /**
+     * @param questionType the questionType to set
+     */
+    public final void setQuestionType(final QuestionType questionType) {
+        this.questionType = questionType;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public final void setName(final String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param questionText the questionText to set
+     */
+    public final void setQuestionText(final QuestionText questionText) {
+        this.questionText = questionText;
+    }
+
+    /**
+     * @param questionHidden the questionHidden to set
+     */
+    public final void setQuestionHidden(final boolean questionHidden) {
+        this.questionHidden = questionHidden;
+    }
+
+    /**
+     * @param feedback the feedback to set
+     */
+    public final void setFeedback(final String feedback) {
+        this.feedback = feedback;
+    }
+
+    /**
+     * @param penalty the penalty to set
+     */
+    public final void setPenalty(final Float penalty) {
+        this.penalty = penalty;
+    }
+
+    /**
+     * @param grade the grade to set
+     */
+    public final void setGrade(final Float grade) {
+        this.grade = grade;
+    }
 
     /**
      * @return le type de la question
      */
-    public abstract QuestionType getQuestionType();
+    public final QuestionType getQuestionType() {
+        return questionType;
+    }
 
     /**
      * @return le nom de la question
      */
-    public abstract String getName();
+    public final String getName() {
+        return name;
+    }
 
     /**
      * @return le texte de la question
      */
-    public abstract QuestionText getQuestionText();
+    public final QuestionText getQuestionText() {
+        return questionText;
+    }
 
     /**
      * @return le feedback de la question
      */
-    public abstract String getGeneralFeedBack();
+    public final String getGeneralFeedBack() {
+        return feedback;
+    }
 
     /**
      * @return la pénalité de la question
      */
-    public abstract Float getPenalty();
+    public final Float getPenalty() {
+        return penalty;
+    }
 
     /**
      * @return le grade de la question
      */
-    public abstract Float getDefaultGrade();
+    public final Float getDefaultGrade() {
+        return grade;
+    }
 
     /**
      * @return si la question est cachée
      */
-    public abstract Boolean isHidden();
+    public final Boolean isHidden() {
+        return questionHidden;
+    }
 
     /**
      * @return l'URL de l'image de la question
      */
-    public abstract String getImageUrl();
+    public final String getImageUrl() {
+        return "";
+    }
 
     /**
      * @return l'image de la question
      */
-    public abstract String getImageBase64();
+    public final String getImageBase64() {
+        return "";
+    }
 
     /**
      * @return la liste des erreurs de la question
      */
-    public abstract List<QuestionError> getErrors();
+    public final List<QuestionError> getErrors() {
+        return null;
+    }
 }
