556976af9d9ca71a53cd375328049e1be78881cf
Stephane P <steph@100pui.com>
2012-05-25 12:01:23 +0200

    Ecriture des tests + fichiers de tests xml [#22]

---
+++ RomanianOpenSourceSimonFull100pui/src/test/java/ups/m2glre/rossf1/parser/CategoryQuestionParserTest.java
@@ -0,0 +1,35 @@
+package ups.m2glre.rossf1.parser;
+
+import java.io.File;
+import junit.framework.TestCase;
+
+import org.jdom.Document;
+import org.jdom.input.SAXBuilder;
+
+import universite.toulouse.moodlexmlapi.core.data.QuestionType;
+import ups.m2glre.rossf1.question.CategoryQuestion;
+
+public class CategoryQuestionParserTest extends TestCase {
+    private CategoryQuestionParser categoryQuestionParser;
+    private CategoryQuestion categoryQuestion;
+    private Document document;
+
+    public void setUp() {
+        categoryQuestionParser = new CategoryQuestionParser();
+        categoryQuestion = null;
+        document = null;
+
+        try {
+            document = new SAXBuilder().build(new File("src/test/quiz-exemple-20120229-0812.xml"));
+            categoryQuestion = (CategoryQuestion) categoryQuestionParser.
+                    parseQuestion(document.getRootElement().getChild("question"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void testCategoryText() {
+        assertEquals("$course$/Défaut pour 1SA3GL1", categoryQuestion.getCategoryText());
+    }
+}
---
+++ RomanianOpenSourceSimonFull100pui/src/test/java/ups/m2glre/rossf1/parser/ClozeQuestionParserTest.java
@@ -0,0 +1,42 @@
+package ups.m2glre.rossf1.parser;
+
+import java.io.File;
+import junit.framework.TestCase;
+
+import org.jdom.Document;
+import org.jdom.input.SAXBuilder;
+
+import universite.toulouse.moodlexmlapi.core.data.QuestionType;
+import ups.m2glre.rossf1.question.ClozeQuestion;
+
+public class ClozeQuestionParserTest extends TestCase {
+    private ClozeQuestionParser ClozeQuestionParser;
+    private ClozeQuestion ClozeQuestion;
+    private Document document;
+
+    public void setUp() {
+        ClozeQuestionParser = new ClozeQuestionParser();
+        ClozeQuestion = null;
+        document = null;
+
+        try {
+            document = new SAXBuilder().build(new File("src/test/TestClozeQuestion.xml"));
+            ClozeQuestion = (ClozeQuestion) ClozeQuestionParser.
+                    parseQuestion(document.getRootElement().getChild("question"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void testQuestionName() {
+        assertTrue(ClozeQuestion.getQuestionText().getText().startsWith(
+                "Cette question comporte du texte dans lequel on a directement"));
+    }
+    public void testQuestionType() {
+        assertEquals(QuestionType.cloze, ClozeQuestion.getQuestionType());
+    }
+    public void testQuestionShuffle() {
+        assertFalse(ClozeQuestion.isAnswerShuffle());
+    }
+}
