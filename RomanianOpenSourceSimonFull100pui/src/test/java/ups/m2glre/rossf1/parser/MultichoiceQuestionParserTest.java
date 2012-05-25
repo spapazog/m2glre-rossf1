ffa408558b5b30bf6446c71b503c89ef90dab26a
Gabriel Diaconescu <gabi.diaconescu@gmail.com>
2012-05-25 16:41:07 +0200

    Wrote the tests for Multiple Choice Question. Also, fixed a bug for IsSingle method, that parsed an integer instead of a String.[Fixed #14]

---
+++ RomanianOpenSourceSimonFull100pui/src/test/java/ups/m2glre/rossf1/parser/MultichoiceQuestionParserTest.java
@@ -0,0 +1,47 @@
+package ups.m2glre.rossf1.parser;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+import org.jdom.Document;
+import org.jdom.input.SAXBuilder;
+
+import universite.toulouse.moodlexmlapi.core.InvalidQuizFormatException;
+import ups.m2glre.rossf1.question.MultichoiceQuestion;
+
+public class MultichoiceQuestionParserTest extends TestCase{
+    private MultichoiceQuestionParser multichoiceQuestionParser;
+    private MultichoiceQuestion multichoiceQuestion;
+    private Document document;
+
+    public void setUp() {
+        multichoiceQuestionParser = new MultichoiceQuestionParser();
+        multichoiceQuestion = null;
+        document = null;
+
+        try {
+            document = new SAXBuilder().build(new File("src/test/TestMultichoiceQuestion.xml"));
+            multichoiceQuestion = (MultichoiceQuestion) multichoiceQuestionParser.
+                    parseQuestion(document.getRootElement().getChild("question"));
+            
+        } catch (Exception e) {}
+    }
+
+    public void testAnswer() {
+        assertEquals(0, multichoiceQuestion.getAnswerFraction());
+        assertEquals("foo", multichoiceQuestion.getAnswerText());
+        assertEquals(true, multichoiceQuestion.isAnswerShuffle());
+    }
+    public void testUnit() {
+        assertEquals(false, multichoiceQuestion.isSingle());
+    }
+    public void testFeedback() {
+        assertEquals("bar", multichoiceQuestion.getFeedbackText());
+        assertEquals("baz", multichoiceQuestion.getGeneralFeedBack());
+        assertEquals("OK", multichoiceQuestion.getCorrectfeedbackText());
+        assertEquals("P", multichoiceQuestion.getPartiallyfeedbackText());
+        assertEquals("KO", multichoiceQuestion.getIncorrectfeedbackText());
+        assertEquals("abc", multichoiceQuestion.getAnswernumbering());
+    }
+}
